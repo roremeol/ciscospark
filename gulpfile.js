8312291f5e1f892e829eb69ce3f4c5c30bde4c28
Joseph Lee <joselee2@cisco.com>
2017-05-03 18:05:47 +0100

    Fix build error for Node 0.10 and 0.12

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var istanbul = require('gulp-istanbul')
 var jsdoc = require('gulp-jsdoc3')
 var del = require('del')
 
-
 var buildDir = 'build'
 var jsSources = [ 'src/**/*.js', 'test/**/*.js' ]
 var testSources = [ 'test/**/*-spec.js' ]
@@ -18,41 +17,41 @@ var babelOptions = {
 }
 function dirFirst (dir) { return (dir.substr(0, dir.indexOf('/')) || '') }
 
-gulp.task('clean', () => {
+gulp.task('clean', function () {
   return del([
     buildDir + '/**/*',
     '!' + buildDir + '/.empty'
   ])
 })
 
-gulp.task('build', ['clean'], () => {
-  jsSources.forEach((src) => {
+gulp.task('build', ['clean'], function () {
+  jsSources.forEach(function (src) {
     gulp.src(src)
       .pipe(babel(babelOptions))
       .pipe(gulp.dest(buildDir + '/' + dirFirst(src)))
   })
 })
 
-gulp.task('pre-test', () => {
+gulp.task('pre-test', function () {
   return gulp.src(jsSources)
   .pipe(istanbul())
   .pipe(istanbul.hookRequire())
 })
 
-gulp.task('test', ['pre-test'], () => {
+gulp.task('test', ['pre-test'], function () {
   return gulp.src(testSources)
     .pipe(mocha())
     .pipe(istanbul.writeReports())
     .pipe(istanbul.enforceThresholds({ thresholds: { global: 60 } }))
 })
 
-gulp.task('docs', (cb) => {
-  const config = require('./jsdocs.json')
+gulp.task('docs', function (cb) {
+  var config = require('./jsdocs.json')
   gulp.src(jsSources, {read: false})
     .pipe(jsdoc(config, cb))
 })
 
-gulp.task('test:build', ['build'], () => {
+gulp.task('test:build', ['build'], function () {
   gulp.src(testBuildSources)
     .pipe(mocha())
 })
